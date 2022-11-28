d274572bdcd8851aa9a242b706eb5e82b73ef203
oseunghyun <ohsh1477@gmail.com>
2022-11-28 12:40:06 +0900

    feat: 메뉴 수정 기능 구현

--- src/js/index.js
+++ src/js/index.js
@@ -9,8 +9,8 @@
 // - [x] 추가되는 메뉴의 아래 마크업은 <ul id="espresso-menu-list" class="mt-3 pl-0"></ul> 안에 삽입해야 한다.
 
 // TODO 메뉴 수정
-// - [ ] 메뉴의 수정 버튼클릭 이벤트를 받고, 메뉴수정하는 모달창이 뜬다.
-// - [ ] 모달창에서 신규 메뉴명을 입력 받고, 확인버튼을 누르면 메뉴가 수정된다.
+// - [x] 메뉴의 수정 버튼클릭 이벤트를 받고, 메뉴수정하는 모달창이 뜬다.
+// - [x] 모달창에서 신규 메뉴명을 입력 받고, 확인버튼을 누르면 메뉴가 수정된다.
 
 // TODO 메뉴 삭제
 // - [ ] 메뉴의 삭제 버튼 클릭 이벤트를 받고, 메뉴 삭제 컨펌 모달창이 뜬다.
@@ -21,6 +21,15 @@ const $ = (selector) => document.querySelector(selector);
 
 function App() {
   // form 태그가 자동으로 전송되는걸 막아준다.
+  $('#espresso-menu-list').addEventListener('click', (e) => {
+    if (e.target.classList.contains('menu-edit-button')) {
+      const $menuName = e.target.closest('li').querySelector('.menu-name');
+      const menuName = $menuName.innerText;
+      const updatedMenuName = prompt('메뉴명을 수정하세요', menuName);
+      $menuName.innerText = updatedMenuName;
+    }
+  });
+
   $('#espresso-menu-form').addEventListener('submit', (e) => {
     e.preventDefault();
   });
